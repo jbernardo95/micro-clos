a03deb3664cb5440ca21eb03206194acbebf90ef
Joao Bernardo <jb.amaro95@gmail.com>
2017-04-23 19:32:58 +0100

    Adds def-class macro with constructor generation

--- src/main.lisp
+++ src/main.lisp
@@ -1,1 +1,8 @@
-(defun test (x) x)
+(defmacro def-class (name &rest params)
+  `(progn
+     (defun ,(constructor-name name) (&key ,@params)
+       (vector ,@params))))
+
+(defun constructor-name (class-name)
+  (intern
+    (format nil "MAKE-~A" class-name)))
