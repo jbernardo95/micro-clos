eddde7ec73b3dcbb644518282c038e60115d2822
Joao Bernardo <jb.amaro95@gmail.com>
2017-04-27 17:45:45 +0100

    Extract costructor and recognizer definition to macros

--- src/main.lisp
+++ src/main.lisp
@@ -1,27 +1,35 @@
 (defmacro def-class (hierarchy &rest params)
   (let ((class-name (get-class-name hierarchy)))
     `(progn
-       ; Generate constructor
-       (defun ,(constructor-name class-name) (&key ,@params)
-          (let ((variables (make-hash-table)))
-            ,@(loop for param in params
-                    collect `(setf (gethash ',param variables) ,param))
-            (vector ',(hierarchy-as-list hierarchy) variables)))
+       ; Define constructor
+       ,`(def-constructor ,hierarchy ,params)
        
-       ; Generate getters 
+       ; Define getters 
        ,@(loop for param in params
                collect `(def-getter ,class-name ,param))
        
-       ; Generate recognizer 
-       (defun ,(recognizer-name class-name) (object)
-         (let ((hierarchy (aref object 0)))
-           (loop for class in hierarchy
-                 do (if (eq class ',class-name)
-                      (return t))))))))
+       ; Define recognizer 
+       ,`(def-recognizer ,hierarchy))))
+
+(defmacro def-constructor (hierarchy params)
+  (let ((class-name (get-class-name hierarchy)))
+    `(defun ,(constructor-name class-name) (&key ,@params)
+       (let ((variables (make-hash-table)))
+         ,@(loop for param in params
+                 collect `(setf (gethash ',param variables) ,param))
+         (vector ',(hierarchy-as-list hierarchy) variables)))))
 
 (defmacro def-getter (class-name param-name)
   `(defun ,(getter-name class-name param-name) (object)
      (gethash ',param-name (aref object 1))))
+
+(defmacro def-recognizer (hierarchy)
+  (let ((class-name (get-class-name hierarchy)))
+    `(defun ,(recognizer-name class-name) (object)
+       (let ((hierarchy (aref object 0)))
+         (loop for class in hierarchy
+               do (if (eq class ',class-name)
+                    (return t)))))))
 
 (defun get-class-name (hierarchy)
   (if (eq (type-of hierarchy) 'CONS)
