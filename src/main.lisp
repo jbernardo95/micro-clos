5dabf0bf0929db97c67220da85a68799d56c5f8d
Joao Bernardo <jb.amaro95@gmail.com>
2017-04-27 17:21:52 +0100

    Adds support for class hierarchy and class recognizers

--- src/main.lisp
+++ src/main.lisp
@@ -1,17 +1,37 @@
-(defmacro def-class (class-name &rest params)
-  `(progn
-     (defun ,(constructor-name class-name) (&key ,@params)
-       (let ((variables (make-hash-table)))
-         ,@(loop for param in params
-                 collect `(setf (gethash ',param variables) ,param))
-         (vector variables)))
+(defmacro def-class (hierarchy &rest params)
+  (let ((class-name (get-class-name hierarchy)))
+    `(progn
+       ; Generate constructor
+       (defun ,(constructor-name class-name) (&key ,@params)
+          (let ((variables (make-hash-table)))
+            ,@(loop for param in params
+                    collect `(setf (gethash ',param variables) ,param))
+            (vector ',(hierarchy-as-list hierarchy) variables)))
+       
+       ; Generate getters 
+       ,@(loop for param in params
+               collect `(def-getter ,class-name ,param))
+       
+       ; Generate recognizer 
+       (defun ,(recognizer-name class-name) (object)
+         (let ((hierarchy (aref object 0)))
+           (loop for class in hierarchy
+                 do (if (eq class ',class-name)
+                      (return t))))))))
 
-     ,@(loop for param in params
-             collect `(generate-getter ,class-name ,param))))
+(defmacro def-getter (class-name param-name)
+  `(defun ,(getter-name class-name param-name) (object)
+     (gethash ',param-name (aref object 1))))
 
-(defmacro generate-getter (class-name param-name)
-  `(defun ,(getter-name class-name param-name) (,class-name)
-     (gethash ',param-name (aref ,class-name 0))))
+(defun get-class-name (hierarchy)
+  (if (eq (type-of hierarchy) 'CONS)
+    (car hierarchy)
+    hierarchy))
+
+(defun hierarchy-as-list (hierarchy)
+  (if (eq (type-of hierarchy) 'CONS)
+    hierarchy
+    (list hierarchy)))
 
 (defun constructor-name (class-name)
   (intern
@@ -20,3 +40,7 @@
 (defun getter-name (class-name param-name)
   (intern
     (format nil "~A-~A" class-name param-name)))
+
+(defun recognizer-name (class-name)
+  (intern
+    (format nil "~A?" class-name)))
