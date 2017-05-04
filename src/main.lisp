092a0f0f4231816873878f4c986c67ab2676b2d5
Joao Bernardo <jb.amaro95@gmail.com>
2017-05-04 12:50:21 +0100

    Moves class structures to an hashtable and protects getters and setters

--- src/main.lisp
+++ src/main.lisp
@@ -1,12 +1,21 @@
+(defvar class-structures (make-hash-table)) ; So that there is no class-structures not defined error
+
 (defmacro def-class (hierarchy &rest params)
   (let ((class-name (get-class-name hierarchy))
         (class-params (get-class-params hierarchy params)))
     `(progn
-       ; Define class structure 
-       (defvar ,(class-structure-variable class-name) '(,@class-params))
+       ; Define class structures hashtable
+       (if (not (boundp 'class-structures))
+         (defvar class-structures (make-hash-table)))
+
+       ; Save class structure 
+       (setf (gethash ',class-name class-structures) ',class-params)
 
        ; Define constructor
        ,`(def-constructor ,class-name ,hierarchy ,class-params)
+
+       ; Define recognizer 
+       ,`(def-recognizer ,class-name)
        
        ; Define getters 
        ,@(loop for param in class-params
@@ -14,10 +23,7 @@
 
        ; Define setters 
        ,@(loop for param in class-params
-               collect `(def-setter ,class-name ,param))
-       
-       ; Define recognizer 
-       ,`(def-recognizer ,class-name))))
+               collect `(def-setter ,class-name ,param)))))
 
 (defmacro def-constructor (class-name hierarchy params)
   `(defun ,(constructor-name class-name) (&key ,@params)
@@ -28,17 +34,24 @@
 
 (defmacro def-getter (class-name param-name)
   `(defun ,(getter-name class-name param-name) (object)
-     (gethash ',param-name (aref object 1))))
+     (if (,(recognizer-name class-name) object)
+       (let ((result nil))
+         (setf result (gethash ',param-name (aref object 1)))
+         result)
+       (print "The given argument is not an instance of the getter class"))))
 
 (defmacro def-setter (class-name param-name)
   `(defun ,(setter-name class-name param-name) (object value)
-     (setf (gethash ',param-name (aref object 1)) value)))
+     (if (,(recognizer-name class-name) object)
+       (setf (gethash ',param-name (aref object 1)) value)
+       (print "The given argument is not an instance of the setter class"))))
 
 (defmacro def-recognizer (class-name)
   `(defun ,(recognizer-name class-name) (object)
      (if (typep object '(simple-vector 2))
-       (let ((hierarchy (aref object 0)))
-         (if (typep hierarchy 'list)
+       (let ((hierarchy (aref object 0))
+             (class-variables (aref object 1)))
+         (if (and (typep hierarchy 'list) (typep class-variables 'hash-table))
            (loop for class in hierarchy
                  do (if (eq class ',class-name)
                       (return t))))))))
@@ -48,12 +61,11 @@
     (car hierarchy)
     hierarchy))
 
-; TODO get alternative to eval
 (defun get-class-params (hierarchy params)
   (if (typep hierarchy 'list)
     (let ((class-params params))
       (loop for class in (cdr hierarchy)
-            do (setf class-params (append class-params (eval (class-structure-variable class)))))
+            do (setf class-params (append class-params (gethash class class-structures))))
       (remove-duplicates class-params))
     params))
 
@@ -61,10 +73,6 @@
   (if (typep hierarchy 'list)
     hierarchy
     (list hierarchy)))
-
-(defun class-structure-variable (class-name)
-  (intern
-    (format nil "~A-CLASS-STRUCTURE" class-name)))
 
 (defun constructor-name (class-name)
   (intern
